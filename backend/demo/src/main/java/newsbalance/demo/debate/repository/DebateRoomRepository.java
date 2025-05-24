2c31b41df03f21bf771ae96c5a1207b4608a5cde
익호 <dldlrgh28@gmail.com>
2025-05-25 00:29:23 +0900

    del

--- backend/demo/src/main/java/newsbalance/demo/debate/repository/DebateRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package newsbalance.demo.debate.repository;
-
-import newsbalance.demo.debate.entity.DebateRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DebateRoomRepository extends JpaRepository<DebateRoom, Long> {
-}
