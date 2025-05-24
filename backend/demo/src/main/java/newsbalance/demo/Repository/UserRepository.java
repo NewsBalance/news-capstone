195ada7ddc7bcf2ca4db25eec2c9875b3c3b9917
익호 <dldlrgh28@gmail.com>
2025-05-24 21:45:47 +0900

    유저정보 수정

--- backend/demo/src/main/java/newsbalance/demo/Repository/UserRepository.java
+++ backend/demo/src/main/java/newsbalance/demo/Repository/UserRepository.java
@@ -4,13 +4,18 @@ import newsbalance.demo.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
-    User findByEmailAndPassword(String email, String password);
+    Optional<User> findByEmailAndPassword(String email, String password);
 
-    User findByNickname(String nickname);
+    Optional<User> findByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
